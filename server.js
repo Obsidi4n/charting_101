dce5256f3eef9be1446157d5b68a1fa850b9ba61
Rohith <rohith.pune@gmail.com>
2015-08-06 02:47:59 +0530

    Server data generation P1 complete

--- server.js
+++ server.js
@@ -12,7 +12,50 @@ app.use('/', router);
 router.get('/',function(req,res){
 	console.log('Sending response');
     res.sendFile('index.html', { root: __dirname});
-})
+});
+
+months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+
+Date.prototype.limitToMarketOpen = function(){
+	//For outside working hours
+	hour = this.getHours();
+	if(hour < 9 || hour >= 17)
+	{
+		if(hour < 9)
+			this.setDate(this.getDate() - 1);
+		this.setHours(17);
+		this.setMinutes(0);
+	}
+
+	//For weekends
+	if(this.getDay() === 0 || this.getDay() === 6)
+	{
+		while( this.getDay() != 5)
+			this.setDate(this.getDate() - 1);	
+		this.setHours(17);
+		this.setMinutes(0);
+	}
+};
+
+Date.prototype.deductTime = function (unit, measure) {
+	if (measure.indexOf('min')===0)
+	{
+		this.setMinutes(this.getMinutes()-1*unit);
+		if(this.getHours() < 9)
+		{
+			this.limitToMarketOpen();
+			this.setMinutes(this.getMinutes()-1*unit);
+		}
+	}
+	else if (measure.indexOf('day')===0)
+		this.setDate(this.getDate()-1*unit);
+	else if (measure.indexOf('week')===0)
+		this.setDate(this.getDate()-7*unit);
+};
+
+Date.prototype.formatDate = function (measure) {
+	return this.getDate() + ' ' + months[this.getMonth()] + ' ' + ("0"+this.getHours()).slice(-2) + ':' + ("0"+this.getMinutes()).slice(-2);
+};
 
 router.post('/dataSource',function(req,res){
 	console.log('Generating data');
@@ -27,7 +70,7 @@ router.post('/dataSource',function(req,res){
 	console.log('Measure '+ measure);
 	console.log('Points to generate: '+count);
 
-	startStamp = clipTimeToScale(date, unit+measure);
+	labelStamp = clipTimeToScale(date, unit, measure);
 	var data = {};
 	var open = Math.random() * 1000;
 	// globalHighest = 0;
@@ -40,59 +83,48 @@ router.post('/dataSource',function(req,res){
 
 		var values = [open, high, low, close];
 		// setGlobals(values);
-		if(i==0)
-			data["T"] = values;
+		
+		if(i===0)
+			label = 'T';
+		else if (i===1)
+			label = labelStamp.formatDate(measure);
 		else
-			data[i] = values;
-
+		{
+			labelStamp.deductTime(unit, measure);
+			label = labelStamp.formatDate(measure);
+		}
+		// console.log(label);
+		data[label] = values;
 		open = close;
 	}
+	console.log(labelStamp);
+	console.log('Data length', Object.keys(data).length)
 	return res.json(data);;
 });
 
-function clipTimeToScale(date,selectedScale)
+function clipTimeToScale(date, unit, measure)
 {
 	date.setTime( date.getTime() + date.getTimezoneOffset()*60*1000 );
 	console.log(date);
 	date.setMilliseconds(0);
 	date.setSeconds(0);
 
+	//If before or after market hours
+	date.limitToMarketOpen();
+	
 	min = date.getMinutes();
 	hour = date.getHours();
 
-	//If before or after market hours
-	if(hour < 9 || hour >= 17)
-	{
-		if(hour < 9)
-			date.setDate(date.getDate() - 1);
-		date.setHours(17);
-		date.setMinutes(0);
-	}
-
-	//For weekends
-	if(date.getDay() === 0 || date.getDay() === 6)
-	{
-		while( date.getDay() != 5)
-			date.setDate(date.getDate() - 1);	
-		date.setHours(17);
-		date.setMinutes(0);
-	}
-	
 	//Round to closest scale multiple
-	if (selectedScale === '15mins')
+	if (measure.indexOf('min')===0)
 	{
-		min = min - (min%15);
-		date.setMinutes(min);
-	}
-	else if (selectedScale === '30mins')
-	{
-		min = min - (min%30);
+		min = min - (min%unit);
 		date.setMinutes(min);
 	}
 	else 
 	{
 		date.setMinutes(0);
-		if (selectedScale === '1hour' || selectedScale === '4hour')
+		if (measure.indexOf('hour')===0)
 		{
 			date.setHours(hour);	//Just to make sure that case does not goto else block below
 		}
@@ -100,14 +132,14 @@ function clipTimeToScale(date,selectedScale)
 		{
 
 			date.setHours(17);
-			if (selectedScale === '1days')
+			if (measure.indexOf('day')===0)
 			{
-				if(hour >= 9)					//Date has already been set back by a day
+				if(hour >= 9)					//Date has already been set back by a day if less then 9
 					date.setDate(date.getDate() - 1);
 				while(date.getDay() === 0 || date.getDay() === 6)
 					date.setDate(date.getDate() - 1);
 			}
-			else if (selectedScale === '1week')
+			else if (measure.indexOf('week')===0)
 			{
 				if(date.getDay()===5)
 					date.setDate(date.getDate() - 7);
