22d4e1d8b947179154c224054ba4d1ebad5c75a8
mohitvyas <mohitvyas92@yahoo.in>
2015-08-06 23:37:35 +0530

    adding tooltip + bar candle chart

--- js/charting.js
+++ js/charting.js
@@ -13,7 +13,74 @@ var offset = scale*3;
 var overflow = scale*5;
 
 var ohlcData;
+var screenData=[];
 var selectedScale = '1days';
+
+canvas.addEventListener('click', function(evt) {
+		mousePos = getMousePos(canvas, evt);
+	 	showTooltip(mousePos, 3);
+    }, false);
+
+canvas.addEventListener('touchstart', function(evt) {
+		pos = {};
+		pos.x=evt.touches[0].clientX;
+		pos.y=evt.touches[0].clientY;
+	    showTooltip(pos,10);
+	    evt.preventDefault();
+	}, false);
+
+function showTooltip(pos, radius){
+		
+        var mouseX=pos.x;
+        var mouseY=pos.y;
+        // var message =  mouseX+ ',' + mouseY;
+        found = false;
+    //     
+        for (var i = 0; i < screenData.length; i++) 
+        {
+        	var point = screenData[i];
+        	var dx = mouseX - point["x"]/scale;
+        	var dy = mouseY - point["y"][3]/scale;
+        	if (dx * dx + dy * dy < radius*radius)
+        	{
+        		writeMessage(i);
+        		found = true;
+        		break;
+        	}
+
+    //         //tooltipSpan.style.left = (point["x"]) +40 +"px";
+    //         //tooltipSpan.style.top = (point["y"][3] + 40) + "px";
+    //         //tipctx.clearRect(0, 0, tipcanvas.width, tipcanvas.height);
+    //         //                  tipCtx.rect(0,0,tipCanvas.width,tipCanvas.height);
+    //         //document.getElementById('tooltip').innerHTML = message
+    //         //tipctx.fillText(message, 5, 15);
+    //         //hit = true;
+    //     }
+
+    	}
+    	if (!found) 
+    		document.getElementById("tooltip").innerHTML='';
+    // }
+    // writeMessage(message);
+}
+
+function getMousePos(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+          x: evt.clientX - rect.left,
+          y: evt.clientY - rect.top
+        };
+}
+
+function writeMessage(index) {
+		var tooltipSpan = document.getElementById('tooltip');
+		var x=index+1
+		var y=Math.round(ohlcData[keys[x]][3],2)
+		message = keys[x] + ',' + y;
+        document.getElementById("tooltip").innerHTML=message;
+        tooltipSpan.style.top = (y + 20) + 'px';
+    	tooltipSpan.style.left = (index+1 + 20) + 'px';
+}
 
 function setGlobals(values)
 {
@@ -139,9 +206,9 @@ function draw()
 	ctx.beginPath();
 	
 	keys = Object.keys(ohlcData);
-	for (var i=plottablePoints;i<2*plottablePoints-1;i++)
+	for (var i=0;i<plottablePoints-1;i++)
 	{
-		xcoord = axesPadding+(i-plottablePoints)*xLabelSeparation;
+		xcoord = axesPadding+(plottablePoints-i)*xLabelSeparation;
 		key = keys[i];
 		var p = {};
 		p["x"] = xcoord;
@@ -149,17 +216,19 @@ function draw()
 		for(var j=0; j<4 ; j++)
 			scaledY.push(canvas.height-axesPadding-(ohlcData[key][j]/yfactor));
 		p["y"] = scaledY;
-		if( chartType==='line' && ohlcData[keys[i+1]])
+		if( chartType==='line' && ohlcData[keys[i-1]])
 		{
 			var p1 = {};
 			p1["x"] = xcoord+xLabelSeparation;
-			p1["y"] = canvas.height-axesPadding-(ohlcData[keys[i+1]][3]/yfactor);
+			p1["y"] = canvas.height-axesPadding-(ohlcData[keys[i-1]][3]/yfactor);
 			drawLine(p, p1, axesPadding);
+			screenData.push(p);
+			drawCricle(p);
 		}
 		else if (i>0)
 		{
 			if(chartType==='bar')
-				drawBar(p, axesPadding, xLabelSeparation);
+				drawBarcandle(p, overflow);
 			else if (chartType==='candlestick')
 				drawCandle(p, xLabelSeparation);
 		}
@@ -229,6 +298,40 @@ function drawBar(point, axesPadding, xLabelSeparation)
 	ctx.fillRect(x-width/2, canvas.height-axesPadding-close, width, close);
 	
 }
+
+function drawBarcandle(point, overflow)
+{
+	var width =1;
+	var x = point["x"];
+	var open  = point["y"][0];
+	var high  = point["y"][1];
+	var low   = point["y"][2];
+	var close = point["y"][3];
+	if(close<=open)  
+	{
+		ctx.strokeStyle = '#6699FF';
+		ctx.fillStyle = "#6699FF";	
+	}				//Reversed operator since open and close are now coordinates from top 
+		
+	else{
+		ctx.strokeStyle = "#FF3366";
+		ctx.fillStyle = '#FF3366';
+	}
+	ctx.beginPath();
+	ctx.moveTo(x,high);
+	ctx.lineTo(x,low);
+	ctx.stroke();
+	ctx.moveTo(x,open);
+	ctx.lineTo(x-overflow,open);
+	ctx.stroke();
+	ctx.moveTo(x,close);
+	ctx.lineTo(x+overflow,close);
+	ctx.stroke();
+	//ctx.fillRect(x-0.5, open-0.5, 1, 1);
+	//ctx.fillRect(x-0.5, close-0.5, 1, 1);
+	
+}
+
 function drawLine(point, point1, axesPadding)
 {
 	var x = point["x"];
@@ -242,6 +345,19 @@ function drawLine(point, point1, axesPadding)
 	ctx.stroke();
 }
 
+function drawCricle(p)
+{
+	var radius=6;
+    ctx.beginPath();
+	ctx.arc(p["x"], p["y"][3], radius, 0, 2 * Math.PI);
+	ctx.fillStyle = '#6699FF';
+	//ctx.globalAlpha=0.5;
+	ctx.fill();
+	//ctx.globalAlpha=1;
+	//ctx.globalAlpha=0.5;
+	//ctx.strokeStyle = "#6699FF";
+	ctx.stroke();
+} 
 function setup(){
 	console.log("setting up");
 
