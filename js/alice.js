7a4cc7cd515593b2ef0f2cdd08af5b854d0b29c2
Rohith <rohith.pune@gmail.com>
2015-08-13 13:39:14 +0530

    Revert "Alice Update- Line, Grid, Labels, ClickChecker"
    This reverts commit dfe1bd4e7fec8aa28410ceaffcfb779edb137543.

--- js/alice.js
+++ js/alice.js
@@ -12,21 +12,14 @@
 		var yStart = this.globals.scale * this.globals.yAxisPadding;
 		var availableWidth = this.canvas.width - yStart;
 		if(availableWidth < 1000)
-		{
-			this.globals.clickRadius = 10;
 			this.globals.plottablePoints = 25;
-		}
 		else
-		{
-			this.globals.clickRadius = 5;
 			this.globals.plottablePoints = 50;
-		}
 
-		this.globals.xLabelSeparation = ((( availableWidth/(this.globals.plottablePoints * this.globals.scale) +0.5) <<1) >>1) ;
+		this.globals.xLabelSeparation = availableWidth/(this.globals.plottablePoints * this.globals.scale) ;
 		// var separation = this.globals.scale * this.globals.xLabelSeparation;
 		// this.globals.plottablePoints = ((((availableWidth/separation)+ 0.5) << 1) >> 1);
-	 	this.globals.gridDrawn = false;
-	 	this.globals.labelsDrawn = false;
+	 	
 		return this;
 	};
 
@@ -35,13 +28,14 @@
 		scale : 2,
 		plottablePoints: 60,
 
-		pointsPerLabel: 4,
+		globalHigh: -1,
+		globalLow: 0,
+
+		xPointsPerLabel: 4,
 		xLabelSeparation : 60,
 		yAxisPadding : 50,
 		xAxisPadding : 25,
 		overflow : 5,
-
-		clickRadius: 5,
 	};
 	
 	Alice.prototype.Data = [],
@@ -57,7 +51,9 @@
 			ctx.globalAlpha = 0.1;
 			ctx.strokeStyle = '#EEE';
 
-			var skipPoints = this.globals.pointsPerLabel;
+			ctx.beginPath();
+
+			var skipPoints = this.globals.xPointsPerLabel;
 			var xLabelSeparation = this.globals.scale * this.globals.xLabelSeparation;
 			var yStart = this.globals.scale * this.globals.yAxisPadding;
 			var xStart = this.globals.scale * this.globals.xAxisPadding;
@@ -86,84 +82,34 @@
 			}
 			ctx.closePath();
 			ctx.globalAlpha = 1;
-
-			this.globals.gridDrawn = true;
 	};
 
 	Alice.prototype.drawLabels = function(){
-		if(this.Data && Object.keys(this.Data).length > 1)
-		{
-			var skipPoints = this.globals.pointsPerLabel;
-			var fontSize = (((this.canvas.width/120) << 1 ) >> 1);
+		var skipPoints = this.globals.xPointsPerLabel;
+		var fontSize = (((this.canvas.width/120) << 1 ) >> 1);
+		var ctx = this.context;
 
-			var xAxisPadding = this.globals.scale * this.globals.xAxisPadding;
-			var yAxisPadding = this.globals.scale * this.globals.yAxisPadding;
-
-			var xStart = this.canvas.width - yAxisPadding;
-			var xLabelHeight = this.canvas.height-xAxisPadding/2;
-			var separation = this.globals.scale * this.globals.xLabelSeparation;
-
-			var ctx = this.context;	
-			ctx.font= fontSize +'px sans-serif';
-			while(ctx.measureText(this.XLabels[1]).width > skipPoints * separation)
-			{
-				fontSize-=0.1;
-				ctx.font= fontSize +'px sans-serif';
-			}
-			ctx.font= ((fontSize<<1)>>1) +'px sans-serif';
-			ctx.fillStyle = '#DDD';
-			ctx.textAlign = 'center';
-
-			for (var i = 0; i < this.globals.plottablePoints/skipPoints; i++) 
-			{
-				var label = this.XLabels[skipPoints*i];
-				ctx.fillText(label, xStart-(i*skipPoints*separation), xLabelHeight);
-			}
-			console.log(i,' labels drawn on X-axis');
-
-			ctx.textAlign = 'left';
-			var currentYLabel = Math.round(this.Data[Object.keys(this.Data)[0]]*100)/100;
-			var currentYPosition = this.ScreenCoords[0].y;
-
-			var labelPadding = this.globals.scale * 5;
-			var padding = 5;
-			var rect = ctx.measureText(currentYLabel);
-			ctx.fillRect(this.canvas.width-yAxisPadding+labelPadding-padding , currentYPosition-padding-20, rect.width+2*padding, 40);
-
-			ctx.fillStyle = '#000';
-			ctx.fillText(currentYLabel, this.canvas.width-yAxisPadding+labelPadding , currentYPosition);
+		var xStart = this.canvas.width - this.globals.scale * this.globals.yAxisPadding;
+		var xLabelHeight = this.canvas.height-this.globals.xAxisPadding/2;
+		var separation = this.globals.scale * this.globals.xLabelSeparation;
 			
-			ctx.fillStyle = '#DDD';
-			var yRange = this.globals.globalHigh - this.globals.globalLow;
-			var increment = yRange / 10;
-			var conversion = this.globals.virtualPixelConversion;
-			var height = this.canvas.height;
-			for(var i=0;i<11;i++)
-			{
-				var label = Math.round(this.globals.globalHigh-i*increment);
-				if ( Math.abs(label-currentYLabel) > 45)
-					ctx.fillText(label, this.canvas.width-yAxisPadding+labelPadding , height- xAxisPadding - label/conversion);
-			}
-			this.globals.labelsDrawn = true;
+		ctx.font= fontSize +'px sans-serif';
+		while(ctx.measureText(this.XLabels[1]).width > skipPoints * separation)
+		{
+			fontSize-=0.1;
+			ctx.font= fontSize +'px sans-serif';
 		}
+		ctx.font= ((fontSize<<1)>>1) +'px sans-serif';
+		ctx.fillStyle = '#DDD';
+		ctx.textAlign = 'center';
+
+		for (var i = 0; i < this.globals.plottablePoints/skipPoints; i++) 
+		{
+			var label = this.XLabels[skipPoints*i];
+			ctx.fillText(label, xStart-(i*skipPoints*separation), xLabelHeight);
+		}
+		console.log(i,' labels drawn');
 	};
-
-	Alice.prototype.clearGrids = function(){
-		var xAxisPadding = this.globals.scale * this.globals.xAxisPadding;
-		var yAxisPadding = this.globals.scale * this.globals.yAxisPadding;
-
-		this.context.clearRect(0,0, this.canvas.width-yAxisPadding, this.canvas.height-xAxisPadding);
-		this.globals.gridDrawn = false;
-	};
-
-	Alice.prototype.clearLabels = function(){
-		var xAxisPadding = this.globals.scale * this.globals.xAxisPadding;
-		var yAxisPadding = this.globals.scale * this.globals.yAxisPadding;
-
-		this.context.clearRect(0,this.canvas.height+this.globals.scale, this.canvas.width, this.canvas.height);
-		this.context.clearRect(this.canvas.width-yAxisPadding+this.globals.scale,0, this.canvas.width, this.canvas.height);
-		this.globals.labelsDrawn = false;
-	}
 
 	Alice.prototype.Tooltip = {
 		draw: function(){
@@ -175,8 +121,8 @@
     		this.Data = data;
     		var keys = Object.keys(data);
     		
-    		var globalHigh = 0;
-    		var globalLow = 0;
+    		var globalHigh = -1;
+    		var globalLow = null;
     		for (var i = 0; i < keys.length; i++)
     		{
     			var value = data[keys[i]];
@@ -207,38 +153,19 @@
     				
 				}
 			}
-			this.globals.globalHigh = Math.ceil(globalHigh/100)*100;
-			this.globals.globalLow = Math.floor(globalLow/100)*100;
-
-		    var dataLimits= this.globals.globalHigh - this.globals.globalLow;
-		    var displayLimits = this.canvas.height - this.globals.xAxisPadding;
-			this.globals.virtualPixelConversion = dataLimits/displayLimits;
+			this.globals.globalHigh = globalHigh+100;
+			this.globals.globalLow = globalLow-100;// > 0 ? globalLow-100 : 0;
     	};
-
-    Alice.prototype.getClosestLabel = function(x){
-	 	var nearestPoint=-1;
-	 	var separation = this.globals.scale * this.globals.xLabelSeparation;
-
-	 	var invertedXCoord= this.canvas.width - x - this.globals.yAxisPadding * this.globals.scale ;
-	 	var modX = invertedXCoord % separation;
-	 	
-	 	if( modX < separation/2)
-	 		nearestPoint=invertedXCoord-modX;
-	 	else
-	 		nearestPoint=invertedXCoord+modX;
-
-	 	var index = (((nearestPoint / separation)<<1)>>1);
-	 	
-	 	return index;
-    };
     
     Alice.prototype.draw = function (data) {
     	if(!data)
     		data = this.Data;
+
+
     };
 
-    Alice.prototype.checkClick = function(x,y){
-    	
+    Alice.prototype.checkDataRange = function(x,y){
+
     };
 
     Alice.prototype.Line = function(data){
@@ -255,7 +182,9 @@
 	    		var xStart = this.canvas.width - yAxisPadding;
 	    		var separation = this.globals.scale * this.globals.xLabelSeparation;
 
-			    var virtualPixelConversion=this.globals.virtualPixelConversion;
+			    var dataLimits= this.globals.globalHigh - this.globals.globalLow;
+			    var displayLimits = this.canvas.height - xAxisPadding;
+			    var yfactor=dataLimits/displayLimits;
 
 			    ctx.lineWidth = this.globals.scale*1;
 				ctx.strokeStyle = '#6699FF';
@@ -267,12 +196,12 @@
 					
 					var p = {};
 					p["x"] = xcoord;
-					p["y"] = ((((this.canvas.height-xAxisPadding-(this.Data[keys[i]]/virtualPixelConversion)) + 0.5) <<1) >>1);
+					p["y"] = ((((this.canvas.height-xAxisPadding-(this.Data[keys[i]]/yfactor)) + 0.5) <<1) >>1);
 					this.ScreenCoords.push(p);
 					
 					var p1 = {};
 					p1["x"] = xcoord-separation;
-					p1["y"] = ((((this.canvas.height-xAxisPadding-(this.Data[keys[i+1]]/virtualPixelConversion)) + 0.5) <<1) >>1);
+					p1["y"] = ((((this.canvas.height-xAxisPadding-(this.Data[keys[i+1]]/yfactor)) + 0.5) <<1) >>1);
 
 					ctx.beginPath();
 
@@ -300,41 +229,144 @@
 			}
 
     	};
-    	this.checkClick = function(position){
-    		var xDisplacement = this.canvas.getBoundingClientRect().left;
-    		var yDisplacement = this.canvas.getBoundingClientRect().top	;
-    		var virtualPixelConversion = this.globals.virtualPixelConversion;
-
-    		var x = (position.x-xDisplacement) * this.globals.scale;
-    		var y = (position.y-yDisplacement) * this.globals.scale;
-    		
-    		var clickRadius = this.globals.scale * this.globals.clickRadius;
-	    	// for (var i = 0; i < this.ScreenCoords.length; i++) 
-	     //    {
-	        	// var point = this.ScreenCoords[i];
-	        	var closestIndex = this.getClosestLabel(x);
-	        	var closestPoint = this.ScreenCoords[closestIndex];
-
-	        	var dx = x - closestPoint.x ;
-	        	var dy = y - closestPoint.y ;
-	        	if (dx * dx + dy * dy < clickRadius*clickRadius)
-	        	{
-	        		var retData = {};
-	        		var key = Object.keys(this.Data)[closestIndex];
-	        		retData[key] = this.Data[key];
-	        		return retData;
-	        	}
-	    	// }
-	    	return false;
+    	this.checkDataRange = function(){
 
     	};
+    };
+	Alice.prototype.Bar=function(data)
+    	{
+	    	console.log('Bar');
+	    	this.draw = function(){
+	    		if(this.Data && Object.keys(this.Data).length>1)
+	    		{
+		    		var keys = Object.keys(this.Data);
+		    		var points = this.globals.plottablePoints;
 
-    	if(data)
+		    		var yAxisPadding = this.globals.scale * this.globals.yAxisPadding;
+					var xAxisPadding = this.globals.scale * this.globals.xAxisPadding;
+
+		    		var xStart = this.canvas.width - yAxisPadding;
+		    		var separation = this.globals.scale * this.globals.xLabelSeparation;
+
+				    var dataLimits= this.globals.globalHigh - this.globals.globalLow;
+				    var displayLimits = this.canvas.height - xAxisPadding;
+				    var yfactor=dataLimits/displayLimits;
+				    
+				    ctx.lineWidth = this.globals.scale*1;
+
+				    //local variables 
+				    var overflow=10;
+				    var open, close, high, low, x;
+
+					for (var i=0; i<points && i<keys.length-1; i++)
+					{
+						var xcoord = xStart-i*separation;
+				
+						var p = {};
+						p["x"] = xcoord;
+						p["y"] = [];
+						for(var j=0; j<4; j++)
+						{
+							p["y"][j] = ((((this.canvas.height-xAxisPadding-(this.Data[keys[i]][j]/yfactor)) + 0.5) <<1) >>1);
+						}
+						this.ScreenCoords.push(p);
+						x = p["x"];
+						open  = p["y"][0];
+						high  = p["y"][1];
+						low   = p["y"][2];
+						close = p["y"][3];
+						if(close<=open)  
+						{
+							ctx.strokeStyle = '#6699FF';
+							ctx.fillStyle = "#6699FF";	
+						}				//Reversed operator since open and close are now coordinates from top 
+							
+						else{
+							ctx.strokeStyle = "#FF3366";
+							ctx.fillStyle = '#FF3366';
+						}
+						ctx.beginPath();
+						ctx.moveTo(x,high);
+						ctx.lineTo(x,low);
+						ctx.stroke();
+						ctx.moveTo(x,open);
+						ctx.lineTo(x-overflow,open);
+						ctx.stroke();
+						ctx.moveTo(x,close);
+						ctx.lineTo(x+overflow,close);
+						ctx.stroke();
+					}
+				}
+			};
+			if(data)
     		this.setData(data);
 
-    	if (this.Data)
-    		this.draw();
-    };       
+	    	if(this.Data)
+	    		this.draw();
+    	};     	
+
+Alice.prototype.CandleStick=function(data)
+    	{
+	    	console.log('CandleStick');
+	    	this.draw = function(){
+	    		if(this.Data && Object.keys(this.Data).length>1)
+	    		{
+		    		var keys = Object.keys(this.Data);
+		    		var points = this.globals.plottablePoints;
+
+		    		var yAxisPadding = this.globals.scale * this.globals.yAxisPadding;
+					var xAxisPadding = this.globals.scale * this.globals.xAxisPadding;
+
+		    		var xStart = this.canvas.width - yAxisPadding;
+		    		var separation = this.globals.scale * this.globals.xLabelSeparation;
+
+				    var dataLimits= this.globals.globalHigh - this.globals.globalLow;
+				    var displayLimits = this.canvas.height - xAxisPadding;
+				    var yfactor=dataLimits/displayLimits;
+				   
+				    ctx.lineWidth = this.globals.scale*1;
+				    ctx.strokeStyle = "#333";
+				    
+				    var width = separation/3;
+					var open, close, high, low, x;
+
+					for (var i=0; i<points && i<keys.length-1; i++)
+					{
+						var xcoord = xStart-i*separation;
+				
+						var p = {};
+						p["x"] = xcoord;
+						p["y"] = [];
+						for(var j=0; j<4; j++)
+							p["y"][j] = ((((this.canvas.height-xAxisPadding-(this.Data[keys[i]][j]/yfactor)) + 0.5) <<1) >>1);
+						this.ScreenCoords.push(p);
+						
+						x = p["x"];
+						open  = p["y"][0];
+						high  = p["y"][1];
+						low   = p["y"][2];
+						close = p["y"][3];
+						if(close<=open)  				//Reversed operator since open and close are now coordinates from top 
+							ctx.fillStyle = "#6699FF";
+						else
+							ctx.fillStyle = "#FF3366";
+						
+						ctx.beginPath();
+						ctx.moveTo(x,high);
+						ctx.lineTo(x,low);
+						ctx.stroke();
+						ctx.closePath();
+
+						ctx.fillRect(x-width/2, open, width, close-open);
+					}
+				}
+			};
+			if(data)
+    		this.setData(data);
+
+	    	if(this.Data)
+	    		this.draw();
+    	};    
 
     window.Alice = Alice;
 }).call(this);
